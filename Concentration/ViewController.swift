14187d671da73f0f75ff932af1f1de2767b53b9f
Abhi Dubal <abhidubal1@yahoo.com>
2018-12-20 15:37:16 -0700

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Abhi Dubal on 12/20/18.
+//  Copyright © 2018 Abhi Dubal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var flipCount = 0
+    {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        let cardNumber = cardButtons.firstIndex(of: sender)!
+        flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        print("cardNumber = \(cardNumber)")
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        
+        /* A way to print out strings in Swift
+        //print("flipCard(withEmoji: \(emoji))")
+        */
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else
+        {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+
+}
+
