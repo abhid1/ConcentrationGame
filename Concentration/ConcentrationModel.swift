c4763d0208bf729e9112c41ebbf3c2d2d33f69a7
Abhi Dubal <abhi@Abhis-MacBook-Pro.local>
2018-12-31 23:23:39 -0700

    Concentration functionality complete. Need to shuffle cards.

--- Concentration/ConcentrationModel.swift
+++ Concentration/ConcentrationModel.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+class Concentration
+{
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int)
+    {
+        if !cards[index].isMatched
+        {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // Check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier
+                {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil;
+            }
+            else
+            {
+                // Either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices
+                {
+                    cards[flipDownIndex].isFaceUp = false;
+                }
+                cards[index].isFaceUp = true;
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int)
+    {
+        for _ in 1...numberOfPairsOfCards
+        {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: Shuffle the cards
+    }
+}
